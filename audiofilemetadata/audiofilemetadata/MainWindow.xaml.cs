f10da4906e73bb4465bc818c51bb8e8735846319
AhmedAhmet <ahmedahmetk@hotmail.co.uk>
2015-03-19 11:03:19 +0000

    Work done so far
    audiofilemeta:
    allows user to select audio file and retrieve metadata
    
    scandirectorytest2: plugin code so far

---
+++ audiofilemetadata/audiofilemetadata/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Win32;
+using TagLib;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+
+namespace audiofilemetadata
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Mp3 Files | *.mp3";
+            if (ofd.ShowDialog() == true)
+            {
+                lblFile.Text = ofd.FileName;
+            }
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            TagLib.File mp3 = TagLib.File.Create(lblFile.Text);
+            lblAlbum.Text = mp3.Tag.Album;
+            lblArtist.Text = GetAllStringsFromArray(mp3.Tag.AlbumArtists, ",");   // Tag.AlbumAritst is a string array 
+            lblGenre.Text = GetAllStringsFromArray(mp3.Tag.Genres, ",");
+            lblTitle.Text = mp3.Tag.Title;
+            lblYear.Text = mp3.Tag.Year.ToString();
+            lblDuration.Text = mp3.Properties.Duration.ToString();
+            //lblLyrics.Text = GetAllStringsFromArray(mp3.Tag.Lyrics, ",");
+   
+        }
+
+        public string GetAllStringsFromArray(string[] strArray, string strDelimeter)
+        {
+            string strFinal = string.Empty;
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                strFinal += strArray[i];
+
+                if (i != strArray.Length - 1)
+                {
+                    strFinal += strDelimeter;
+                }
+            }
+            return strFinal;
+        }
+             
+ 
+
+
+    }
+}
---
+++ scanDirectoryTest2/scanDirectoryTest2/ScanDir.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TagLib;
+using Mp3Lib;
+using IUnification.Models.Interfaces;
+
+namespace scanDirectoryTest2
+{
+    class ScanDir : IMetadataContainer
+    {
+        public static void Main(string[] args)
+        {
+            DirectoryInfo di = new DirectoryInfo("E:\\");
+            FullDirList(di, "*.mp3");
+
+            Console.WriteLine("Scan Complete");
+           
+            Console.Read();
+        }
+
+        public static List<FileInfo> files = new List<FileInfo>();  
+        public static List<DirectoryInfo> folders = new List<DirectoryInfo>(); 
+        public static void FullDirList(DirectoryInfo dir, string searchPattern)
+        {
+
+            try
+            {
+                foreach (FileInfo f in dir.GetFiles(searchPattern))
+                {
+                    Console.WriteLine("File {0}", f.FullName);
+                    files.Add(f);
+
+
+                    //Using ID3 lib
+
+                    //Using TagLib
+                    TagLib.File file = TagLib.File.Create(f.FullName);
+                    string Title = file.Tag.Title;
+                    string[] Artist = file.Tag.Performers;
+                    string Album = file.Tag.Album;
+                    uint Year = file.Tag.Year;
+                    TimeSpan Duration = file.Properties.Duration;
+                    //Uri Datasource = file.Properties;
+
+
+   
+
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("Directory {0}  \n could not be accessed!!!!", dir.FullName);
+                return;  
+            }
+
+  
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                folders.Add(d);
+                FullDirList(d, searchPattern);
+            }
+            
+
+
+        }
+    }
+}
